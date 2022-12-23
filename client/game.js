95034c7fe6259d8bb48ed487848f8ef4ac1eebf8
Johannes Niemelä <johannes@niemela.se>
2022-12-23 11:06:33 -0600

    added pawn movement

--- client/game.js
+++ client/game.js
@@ -1,6 +1,19 @@
+let selectedTile = null
+let path = []
+let hasMoved = false
+
+let tiles = []
+let isGoldsTurn = false
+
+const config = {
+  allowUndo: false,
+}
+
 class Tile {
-  constructor(tileDiv) {
+  constructor(tileDiv, x, y) {
     this.tileDiv = tileDiv
+    this.x = x
+    this.y = y
     this.pawnStack = []
     this.liftCount = 0
 
@@ -8,32 +21,140 @@ class Tile {
   }
 
   onClick(e) {
-    if (e.shiftKey) {
-      if (this.liftCount > 0) {
-        this.pawnStack[this.pawnStack.length-this.liftCount].unlift()
-        this.liftCount--
+    if (this.pawnStack.length > 0 && this.pawnStack[this.pawnStack.length-1].isGold !== isGoldsTurn) {
+      return
+    }
+
+    if (selectedTile && !(selectedTile === this)) {
+      const isReturning = this === path[path.length-1] && config.allowUndo
+      const topChip = this.pawnStack[this.pawnStack.length-1];
+      if (this.pawnStack.length > 0 && !isReturning && !(topChip && topChip.isEnergy)) {
+        selectedTile.deselect()
       } else {
-        this.liftCount = this.pawnStack.length
-        this.pawnStack.forEach(pawn => pawn.lift())
+        if (selectedTile.canMoveTo(this.x, this.y)) {
+          if (hasMoved && !isReturning) return
+          if (isReturning) {
+            path.pop()
+            hasMoved = false
+          } else {
+            const isFreeMove = selectedTile.isFreeMove(this.x, this.y)
+            if (!isFreeMove) {
+              if (path.length > 0) return
+              hasMoved = true
+            }
+          }
+          const movedPawns = []
+          const selectedPawns = selectedTile.liftCount
+          for (let i = 0; i < selectedPawns; i++) {
+            movedPawns.push(selectedTile.popPawn())
+          }
+          for (let i = selectedPawns - 1; i >= 0; i--) {
+            this.pushPawn(movedPawns[i])
+          }
+          if (!isReturning) path.push(selectedTile)
+          selectedTile = this
+          return
+        } else {
+          if (hasMoved || path.length > 0) {
+            return
+          } else {
+            selectedTile.deselect()
+          }
+        }
       }
-    } else {
+    }
+    selectedTile = this
+
+    if (e.shiftKey) {
       if (this.liftCount < this.pawnStack.length) {
         this.liftCount++
         this.pawnStack[this.pawnStack.length-this.liftCount].lift()
       } else {
         this.deselect()
       }
+    } else {
+      if (this.liftCount > 0) {
+        this.pawnStack[this.pawnStack.length-this.liftCount].unlift()
+        this.liftCount--
+        if (this.liftCount === 0) {
+          this.deselect();
+        }
+      } else {
+        this.liftCount = this.pawnStack.length
+        this.pawnStack.forEach(pawn => pawn.lift())
+      }
     }
+  }
+
+  canMoveTo(x, y) {
+    if (Math.abs(x-this.x) <= 1 && Math.abs(y-this.y) <= 1) {
+      return true
+    }
+    return false
+  }
+
+  isFreeMove(x, y) {
+    const lastTile = path[path.length-1]
+    let lastTopChip;
+    if (lastTile) {
+      lastTopChip = lastTile.pawnStack[lastTile.pawnStack.length-1]
+    }
+    const newTopChip = this.pawnStack[this.pawnStack.length-this.liftCount-1]
+    const isOrthogonal = !(this.x !== x && this.y !== y)
+    console.log(newTopChip, isOrthogonal)
+    if (newTopChip && newTopChip.isEnergy) {
+      if (lastTile) {
+        if (isOrthogonal) {
+          return newTopChip.isDark && (
+            !lastTopChip.isDark || (
+              lastTile.x === x || lastTile.y === y
+            )
+          )
+        } else {
+          return !newTopChip.isDark && (
+            lastTopChip.isDark || !(
+              lastTile.x === x || lastTile.y === y
+            )
+          )
+        }
+      } else {
+        return newTopChip.isDark === isOrthogonal
+      }
+    }
+    return false
   }
   
   deselect() {
     this.liftCount = 0
     this.pawnStack.forEach(pawn => pawn.unlift())
+    selectedTile = null
+    if (path.length > 0) {
+      passTurn()
+    }
+    path = []
   }
 
-  addPawn(pawn) {
+  pushPawn(pawn) {
+    if (this.pawnStack.length === 0) {
+      pawn.setBottom(true)
+    }
     this.pawnStack.push(pawn)
     this.tileDiv.appendChild(pawn.element)
+    if (this.liftCount > 0 || pawn.lifted) {
+      this.liftCount++
+    }
+  }
+
+  popPawn() {
+    const pawn = this.pawnStack.pop()
+    this.tileDiv.removeChild(pawn.element)
+    if (this.pawnStack.length === 0) {
+      pawn.setBottom(false)
+    }
+    if (this.liftCount > 0) {
+      this.liftCount--
+    }
+    return pawn
   }
 }
 
@@ -52,6 +173,14 @@ class BoardPiece {
     this.element.classList.remove('lifted')
     this.lifted = false
   }
+
+  setBottom(state) {
+    if (state) {
+      this.element.classList.add('bottom')
+    } else {
+      this.element.classList.remove('bottom')
+    }
+  }
 }
 
 class Chip extends BoardPiece {
@@ -63,6 +192,7 @@ class Chip extends BoardPiece {
     element.src = `assets/pieces/${type}.png`
     div.appendChild(element)
     super(div)
+    this.isChip = true
   }
 }
 
@@ -77,6 +207,7 @@ class Energy extends Chip {
     super(`${isDark ? 'dark' : 'light'}${isGold ? 'gold' : 'red'}`)
     this.isDark = isDark
     this.isGold = isGold
+    this.isEnergy = true
   }
 }
 
@@ -88,6 +219,7 @@ class Pawn extends BoardPiece {
     element.src = `assets/pieces/${isGold ? 'gold': 'red'}${type}.png`
     super(element)
     this.isGold = isGold
+    this.isChip = false
   }
 }
 
@@ -121,11 +253,20 @@ class King extends Pawn {
   }
 }
 
+function passTurn() {
+  isGoldsTurn = !isGoldsTurn
+  selectedTile = null
+  path = []
+  hasMoved = false
+  document.getElementById('turn').innerText = `${isGoldsTurn ? 'Gold' : 'Red'} is taking their turn...`
+}
+
 function resetBoard() {
   const board = document.getElementById('board')
   board.innerHTML = ''
 
-  const tiles = []
+  tiles = []
+  isGoldsTurn = false
 
   for (let i = 0; i < 10; i++) {
     const row = document.createElement('div')
@@ -135,7 +276,7 @@ function resetBoard() {
       const tileDiv = document.createElement('div')
       tileDiv.classList.add('boardTile')
       tileDiv.classList.add((j + i) % 2 ? 'darkTile' : 'lightTile')
-      tiles[i].push(new Tile(tileDiv))
+      tiles[i].push(new Tile(tileDiv, i, j))
       row.appendChild(tileDiv)
     }
     board.appendChild(row)
@@ -143,95 +284,106 @@ function resetBoard() {
 
   /* Add Gold's Energy */
   // Dark
-  tiles[0][0].addPawn(new Energy(true, true))
-  tiles[0][0].addPawn(new Energy(true, true))
-  tiles[0][0].addPawn(new Energy(true, true))
-  tiles[0][9].addPawn(new Energy(true, true))
-  tiles[0][9].addPawn(new Energy(true, true))
-  tiles[0][9].addPawn(new Energy(true, true))
-  tiles[0][1].addPawn(new Energy(true, true))
-  tiles[0][1].addPawn(new Energy(true, true))
-  tiles[0][8].addPawn(new Energy(true, true))
-  tiles[0][8].addPawn(new Energy(true, true))
-  tiles[0][2].addPawn(new Energy(true, true))
-  tiles[0][7].addPawn(new Energy(true, true))
-  tiles[0][5].addPawn(new Energy(true, true))
+  tiles[0][0].pushPawn(new Energy(true, true))
+  tiles[0][0].pushPawn(new Energy(true, true))
+  tiles[0][0].pushPawn(new Energy(true, true))
+  tiles[0][9].pushPawn(new Energy(true, true))
+  tiles[0][9].pushPawn(new Energy(true, true))
+  tiles[0][9].pushPawn(new Energy(true, true))
+  tiles[0][1].pushPawn(new Energy(true, true))
+  tiles[0][1].pushPawn(new Energy(true, true))
+  tiles[0][8].pushPawn(new Energy(true, true))
+  tiles[0][8].pushPawn(new Energy(true, true))
+  tiles[0][2].pushPawn(new Energy(true, true))
+  tiles[0][7].pushPawn(new Energy(true, true))
+  tiles[0][5].pushPawn(new Energy(true, true))
   // Light
-  tiles[0][3].addPawn(new Energy(false, true))
-  tiles[0][3].addPawn(new Energy(false, true))
-  tiles[0][3].addPawn(new Energy(false, true))
-  tiles[0][6].addPawn(new Energy(false, true))
-  tiles[0][6].addPawn(new Energy(false, true))
-  tiles[0][6].addPawn(new Energy(false, true))
-  tiles[0][2].addPawn(new Energy(false, true))
-  tiles[0][2].addPawn(new Energy(false, true))
-  tiles[0][7].addPawn(new Energy(false, true))
-  tiles[0][7].addPawn(new Energy(false, true))
-  tiles[0][1].addPawn(new Energy(false, true))
-  tiles[0][8].addPawn(new Energy(false, true))
-  tiles[0][5].addPawn(new Energy(false, true))
+  tiles[0][3].pushPawn(new Energy(false, true))
+  tiles[0][3].pushPawn(new Energy(false, true))
+  tiles[0][3].pushPawn(new Energy(false, true))
+  tiles[0][6].pushPawn(new Energy(false, true))
+  tiles[0][6].pushPawn(new Energy(false, true))
+  tiles[0][6].pushPawn(new Energy(false, true))
+  tiles[0][2].pushPawn(new Energy(false, true))
+  tiles[0][2].pushPawn(new Energy(false, true))
+  tiles[0][7].pushPawn(new Energy(false, true))
+  tiles[0][7].pushPawn(new Energy(false, true))
+  tiles[0][1].pushPawn(new Energy(false, true))
+  tiles[0][8].pushPawn(new Energy(false, true))
+  tiles[0][5].pushPawn(new Energy(false, true))
   
   /* Add Red's Energy */
   // Dark
-  tiles[9][0].addPawn(new Energy(true, false))
-  tiles[9][0].addPawn(new Energy(true, false))
-  tiles[9][0].addPawn(new Energy(true, false))
-  tiles[9][9].addPawn(new Energy(true, false))
-  tiles[9][9].addPawn(new Energy(true, false))
-  tiles[9][9].addPawn(new Energy(true, false))
-  tiles[9][1].addPawn(new Energy(true, false))
-  tiles[9][1].addPawn(new Energy(true, false))
-  tiles[9][8].addPawn(new Energy(true, false))
-  tiles[9][8].addPawn(new Energy(true, false))
-  tiles[9][2].addPawn(new Energy(true, false))
-  tiles[9][7].addPawn(new Energy(true, false))
-  tiles[9][5].addPawn(new Energy(true, false))
+  tiles[9][0].pushPawn(new Energy(true, false))
+  tiles[9][0].pushPawn(new Energy(true, false))
+  tiles[9][0].pushPawn(new Energy(true, false))
+  tiles[9][9].pushPawn(new Energy(true, false))
+  tiles[9][9].pushPawn(new Energy(true, false))
+  tiles[9][9].pushPawn(new Energy(true, false))
+  tiles[9][1].pushPawn(new Energy(true, false))
+  tiles[9][1].pushPawn(new Energy(true, false))
+  tiles[9][8].pushPawn(new Energy(true, false))
+  tiles[9][8].pushPawn(new Energy(true, false))
+  tiles[9][2].pushPawn(new Energy(true, false))
+  tiles[9][7].pushPawn(new Energy(true, false))
+  tiles[9][5].pushPawn(new Energy(true, false))
   // Light
-  tiles[9][3].addPawn(new Energy(false, false))
-  tiles[9][3].addPawn(new Energy(false, false))
-  tiles[9][3].addPawn(new Energy(false, false))
-  tiles[9][6].addPawn(new Energy(false, false))
-  tiles[9][6].addPawn(new Energy(false, false))
-  tiles[9][6].addPawn(new Energy(false, false))
-  tiles[9][2].addPawn(new Energy(false, false))
-  tiles[9][2].addPawn(new Energy(false, false))
-  tiles[9][7].addPawn(new Energy(false, false))
-  tiles[9][7].addPawn(new Energy(false, false))
-  tiles[9][1].addPawn(new Energy(false, false))
-  tiles[9][8].addPawn(new Energy(false, false))
-  tiles[9][5].addPawn(new Energy(false, false))
+  tiles[9][3].pushPawn(new Energy(false, false))
+  tiles[9][3].pushPawn(new Energy(false, false))
+  tiles[9][3].pushPawn(new Energy(false, false))
+  tiles[9][6].pushPawn(new Energy(false, false))
+  tiles[9][6].pushPawn(new Energy(false, false))
+  tiles[9][6].pushPawn(new Energy(false, false))
+  tiles[9][2].pushPawn(new Energy(false, false))
+  tiles[9][2].pushPawn(new Energy(false, false))
+  tiles[9][7].pushPawn(new Energy(false, false))
+  tiles[9][7].pushPawn(new Energy(false, false))
+  tiles[9][1].pushPawn(new Energy(false, false))
+  tiles[9][8].pushPawn(new Energy(false, false))
+  tiles[9][5].pushPawn(new Energy(false, false))
 
   /* Add Shields */
-  tiles[0][4].addPawn(new Shield())
-  tiles[0][4].addPawn(new Shield())
-  tiles[0][4].addPawn(new Shield())
-  tiles[9][4].addPawn(new Shield())
-  tiles[9][4].addPawn(new Shield())
-  tiles[9][4].addPawn(new Shield())
-  tiles[0][5].addPawn(new Shield())
-  tiles[9][5].addPawn(new Shield())
+  tiles[0][4].pushPawn(new Shield())
+  tiles[0][4].pushPawn(new Shield())
+  tiles[0][4].pushPawn(new Shield())
+  tiles[9][4].pushPawn(new Shield())
+  tiles[9][4].pushPawn(new Shield())
+  tiles[9][4].pushPawn(new Shield())
+  tiles[0][5].pushPawn(new Shield())
+  tiles[9][5].pushPawn(new Shield())
+  // Tower Shields
+  tiles[1][4].pushPawn(new Shield())
+  tiles[1][4].pushPawn(new Shield())
+  tiles[1][5].pushPawn(new Shield())
+  tiles[1][5].pushPawn(new Shield())
+  tiles[8][4].pushPawn(new Shield())
+  tiles[8][4].pushPawn(new Shield())
+  tiles[8][5].pushPawn(new Shield())
+  tiles[8][5].pushPawn(new Shield())
 
   /* Add Pawns */
   // Light & Heavy Pawns
   for (let i = 0; i < 4; i++) {
-    tiles[1][i].addPawn(new LightPawn(true))
-    tiles[8][i].addPawn(new LightPawn(false))
-    tiles[1][9-i].addPawn(new LightPawn(true))
-    tiles[8][9-i].addPawn(new LightPawn(false))
-    tiles[0][i].addPawn(new HeavyPawn(true))
-    tiles[9][i].addPawn(new HeavyPawn(false))
-    tiles[0][9-i].addPawn(new HeavyPawn(true))
-    tiles[9][9-i].addPawn(new HeavyPawn(false))
+    // tiles[1][i].pushPawn(new LightPawn(true))
+    // tiles[8][i].pushPawn(new LightPawn(false))
+    // tiles[1][9-i].pushPawn(new LightPawn(true))
+    // tiles[8][9-i].pushPawn(new LightPawn(false))
+    tiles[0][i].pushPawn(new HeavyPawn(true))
+    tiles[9][i].pushPawn(new HeavyPawn(false))
+    tiles[0][9-i].pushPawn(new HeavyPawn(true))
+    tiles[9][9-i].pushPawn(new HeavyPawn(false))
   }
   // Towers
-  tiles[1][4].addPawn(new Tower(true))
-  tiles[1][5].addPawn(new Tower(true))
-  tiles[8][4].addPawn(new Tower(false))
-  tiles[8][5].addPawn(new Tower(false))
+  tiles[1][4].pushPawn(new Tower(true))
+  tiles[1][5].pushPawn(new Tower(true))
+  tiles[8][4].pushPawn(new Tower(false))
+  tiles[8][5].pushPawn(new Tower(false))
   // Queens
-  tiles[0][5].addPawn(new Queen(true))
-  tiles[9][5].addPawn(new Queen(false))
+  tiles[0][5].pushPawn(new Queen(true))
+  tiles[9][5].pushPawn(new Queen(false))
   // Kings
-  tiles[0][4].addPawn(new King(true))
-  tiles[9][4].addPawn(new King(false))
+  tiles[0][4].pushPawn(new King(true))
+  tiles[9][4].pushPawn(new King(false))
+
+  passTurn()
 }
